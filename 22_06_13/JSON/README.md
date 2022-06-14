55711b6714826bfd4440b09ffa516844127d1dae
Farid <esserhane@gmail.com>
2022-06-14 09:04:53 +0200

    22_06_14

--- 22_06_13/JSON/README.md
+++ 22_06_13/JSON/README.md
@@ -3,3 +3,23 @@ création 2009
 JIT Just In Time
 Apach/php => serveur classique Thread => context => RAM =>pile dexecution
 NodeJS il est mono-thread 
+
+docker images
+docker ps -a
+whoami
+apk add node
+node -v
+v16.15.0
+apk add nodejs
+apk add npm
+npm -v
+8.10.0
+
+MongoDB
+Express
+React
+Node
+
+
+docker exec -it 7b /bin/sh 
+7b c'est le id du container
--- 22_06_13/src/js/main.js
+++ 22_06_13/src/js/main.js
@@ -27,3 +27,27 @@ console.table(main_medoc_list)
 
 // console.log(pharm);
 //localStorage.setItem("data", JSON.stringify(pharm));
+
+/* fichier pierre
+
+        // emulate external storage 
+        let main_client_list = [];
+        let main_medoc_list = [];
+        // fixture 
+        let first_names = ["roger","michel","emile","louise","renée"];
+        let last_names = ["Mouloud","Sanchez","Giocanti","James","Merkel","Ikea"];
+        let medoc_names = ["doliprane","temesta","prozac","ketamine","viagra","betadine","smecta","vogalene"];
+        let medoc_condition = ["20","250","3000"]
+        // generate client_list 
+        for(i=0;i<20;i++){
+            let myname = first_names[Math.floor(Math.random()*first_names.length)] + " " + last_names[Math.floor(Math.random()*last_names.length)];
+            main_client_list[i] = new Client(i,myname,100);
+        }
+        // generate medoc list 
+        for(j=0;j<30;j++){
+            let mymedoc = medoc_names[Math.floor(Math.random()*medoc_names.length)] + "-" + medoc_condition[Math.floor(Math.random()*medoc_condition.length)];
+            main_medoc_list[j] = new Medoc(j,mymedoc,(Math.random()*100).toFixed(2),100);
+        }
+
+
+*/
